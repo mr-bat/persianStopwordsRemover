e1ab332e4043087ce095efad4d1762d2bc4286a9
Abtin Bateni <abtinbateni@gmail.com>
2018-05-11 13:12:24 +0430

    finished code, NOT tested yet, switched to npm

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-import string
-s = "ASdf"
-s.translate(None, string.punctuation)
-text_words = s.split()
-final_words = []
+import {readFileSync, writeFileSync} from fs;
+const stopwords = require('stopwords-fa'); // array of stopwords
+const args = process.argv.slice(2);
 
-for w in words:
+if (args.length != 2) {
+	console.log("Usage: npm start inputFile outputFile");
+}
+else {
+	str = readFileSync(args[0]);
+	str = str.replace(/[^\w\s]|_/g, "")
+         .replace(/\s+/g, " ");
+	originalWords = str.split(" ");
 	
+	finalWords = originalWords.filter(word => !stopwords.includes(word));
+	writeFileSync(args[1], finalWords.join(' '));
+	console.log('Succesful!');
+}
