8db004338e0b5ed3354f02c2689b4a5383636f6c
Abtin Bateni <abtinbateni@gmail.com>
2018-05-11 14:14:37 +0430

    fixed punctuation bug

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-import {readFileSync, writeFileSync} from fs;
+const fs = require('fs');
 const stopwords = require('stopwords-fa'); // array of stopwords
+const removePuncs = require('remove-punctuation');
 const args = process.argv.slice(2);
 
 if (args.length != 2) {
 	console.log("Usage: npm start inputFile outputFile");
 }
 else {
-	str = readFileSync(args[0]);
-	str = str.replace(/[^\w\s]|_/g, "")
-         .replace(/\s+/g, " ");
+	str = fs.readFileSync(args[0], "utf8");
+	//console.log(str);
+	console.log(removePuncs(str));
+	//str = str.replace(/[^\w\s]|_/g, "")
+    //     .replace(/\s+/g, " ");
+	str = removePuncs(str);
 	originalWords = str.split(" ");
 	
 	finalWords = originalWords.filter(word => !stopwords.includes(word));
-	writeFileSync(args[1], finalWords.join(' '));
+	fs.writeFileSync(args[1], finalWords.join(' '));
 	console.log('Succesful!');
 }
